c93d12643bbe963fb52910396318652ed458ce92
Takatoshi-Miura <takatoshi052031@gmail.com>
2025-03-11 21:47:01 +0900

    初回コミット

---
+++ SportsNote_iOS/SportsNote_iOSApp.swift
@@ -0,0 +1,17 @@
+//
+//  SportsNote_iOSApp.swift
+//  SportsNote_iOS
+//
+//  Created by Takatoshi Miura on 2025/03/11.
+//
+
+import SwiftUI
+
+@main
+struct SportsNote_iOSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ SportsNote_iOSTests/SportsNote_iOSTests.swift
@@ -0,0 +1,16 @@
+//
+//  SportsNote_iOSTests.swift
+//  SportsNote_iOSTests
+//
+//  Created by Takatoshi Miura on 2025/03/11.
+//
+
+import Testing
+
+struct SportsNote_iOSTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
