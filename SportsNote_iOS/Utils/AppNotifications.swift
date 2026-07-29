d65956bf82a9220f28768e779b587f1d090c42bc
Takatoshi-Miura <takatoshi052031@gmail.com>
2026-07-30 00:46:09 +0900

    refactor: MoveToToday通知名をAppNotifications.swiftの定数パターンに統一
    TodayButton/CalendarViewの計3箇所に生文字列直書きされていたNSNotification.Name("MoveToToday")を、
    AppNotifications.swiftのNotification.Name拡張に定義したmoveToToday定数経由に統一した。
    
    Closes #87

--- SportsNote_iOS/Utils/AppNotifications.swift
+++ SportsNote_iOS/Utils/AppNotifications.swift
@@ -10,4 +10,7 @@ extension Notification.Name {
 
     /// アプリを再初期化する必要がある際の通知
     static let shouldReinitializeApp = Notification.Name("shouldReinitializeApp")
+
+    /// 「今日」ボタンタップ時にカレンダーを今日の月へ移動させるための通知
+    static let moveToToday = Notification.Name("MoveToToday")
 }
