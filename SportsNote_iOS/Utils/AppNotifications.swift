2a1498b39076c387b7783fef9773b52a1aca71e2
Takatoshi-Miura <takatoshi052031@gmail.com>
2026-07-29 18:58:42 +0900

    refactor: 未使用のdidLogout通知定義とNotePageView.readOnlyRowを削除
    grepで呼び出し・observe箇所が定義行以外に存在しないことを確認した
    デッドコード2箇所を削除した。
    
    Closes #79

--- SportsNote_iOS/Utils/AppNotifications.swift
+++ SportsNote_iOS/Utils/AppNotifications.swift
@@ -2,9 +2,6 @@ import Foundation
 
 /// アプリケーション内で使用する通知名の定義
 extension Notification.Name {
-    /// ログアウト時にViewModelをクリーンアップするための通知
-    static let didLogout = Notification.Name("didLogout")
-
     /// データがクリアされた際の通知
     static let didClearAllData = Notification.Name("didClearAllData")
 
